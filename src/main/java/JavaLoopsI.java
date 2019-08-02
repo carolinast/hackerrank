6e456cabd60c213e94c1f7c6f5c2887fd41f54ee
Karolina <krk.carolina@gmail.com>
2019-08-02 08:52:20 +0200

    Initial commit

---
+++ src/main/java/JavaLoopsI.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+public class JavaLoopsI {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+        int result = 0;
+        for (int i = 1; i <= 10; i++) {
+            result = N * i;
+            System.out.println(N + " x " + i + " = " + result);
+        }
+    }
+}
