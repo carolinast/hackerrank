721536fcdc741d2cf34ad886c4678f0750de9300
Karolina <krk.carolina@gmail.com>
2019-08-08 21:19:20 +0200

    new solutions

---
+++ src/main/java/JavaEndOfFile.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class JavaEndOfFile{
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 1; scan.hasNext() ; i++) {
+            System.out.println(i + " " + scan.nextLine());
+        }
+    }
+}
+
+
