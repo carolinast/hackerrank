2c2083dc24648e2a8ad0b7cf95ae32c435b9b1e5
Karolina <krk.carolina@gmail.com>
2019-09-04 09:19:48 +0200

    one correction

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,19 +1,7 @@
-import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        while (scan.hasNext()){
-            int[] a = new int[]
-        }
-            scan.close();
-
-        // Prints each sequential element in array a
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
-        }
     }
 }
