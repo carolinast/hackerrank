9816963cd31b5d9fb2843d883efae5d6e409d0ae
Karolina <krk.carolina@gmail.com>
2019-09-18 15:57:56 +0200

    3 new classes

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
 
 public class Solution {
-
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        scan.nextLine();
+        String word;
+
+        for (int i = 0; i < number; i++) {
+            word = scan.next();
+            for (int j = 0; j < word.length(); j++) {
+                if (j % 2 == 0) {
+                    System.out.print(word.charAt(j));
+                }
+            }
+            System.out.print(" ");
+            for (int j = 0; j < word.length(); j++) {
+                if (j % 2 == 1) {
+                    System.out.print(word.charAt(j));
+                }
+            }
+            System.out.println();
+        }
     }
 }
---
+++ src/main/java/TipAndTax.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+
+public class TipAndTax {
+
+    // Complete the solve function below.
+    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        double tip;
+        double tax;
+        double result;
+
+        tip = meal_cost * tip_percent / 100;
+        tax = meal_cost * tax_percent / 100;
+
+        result = meal_cost + tip + tax;
+
+        System.out.println(Math.round(result));
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double meal_cost = scanner.nextDouble();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tip_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tax_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        solve(meal_cost, tip_percent, tax_percent);
+
+        scanner.close();
+    }
+}
